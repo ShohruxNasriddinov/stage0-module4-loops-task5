c30d6155139e99ecd7ac9afca204731d6537a37e
ShohruxNasriddinov <shohshoh1104@gmail.com>
2022-12-13 14:15:46 +0500

    commit loops task5

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        for (int i = 0; i < sideLength; i++) {
+            if (i==sideLength/2||i==sideLength){
+                for (int j = 0; j < sideLength; j++) {
+                    System.out.print("8");
+                }
+            }else {
+                for (int j = 0; j < sideLength; j++) {
+                    if (j==sideLength/2||j==sideLength){
+                        System.out.print("8");
+                    }else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
