d393ef2de78f9ec9d2725b91436041f569e71317
ShohruxNasriddinov <shohshoh1104@gmail.com>
2022-12-13 15:06:15 +0500

    commit3 loops task5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,23 +3,24 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
         int i, j, k;
-        for (i = 1; i <= height; i++) {
 
-            for (k = 1; k < i; k++)
-                System.out.print(" ");
+        for (i = 1; i <= height; i+=2) {
+            for (j = 1; j < i; j+=2) {
+                System.out.print(' ');
+            }
 
-            for (j = i; j <= height; j++)
+            for (k = i; k <= height; k++) {
                 System.out.print("8");
-
+            }
             System.out.println();
         }
-        for (i = height - 1; i >= 1; i--) {
-
-            for (k = 1; k < i; k++)
-                System.out.print(" ");
-
-            for (j = i; j <= height; j++)
+        for (i = height - 1; i >= 1; i-=2) {
+            for (j = 1; j < i; j+=2) {
+                System.out.print(' ');
+            }
+            for (k = i; k <= height; k++) {
                 System.out.print("8");
+            }
 
             System.out.println();
         }
